ad8766e131cb2efeee19caf036b2443ad7b80f7b
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2014-09-23 11:04:50 +0100

    Updated to take an argument indicating the simulator

--- run.py
+++ run.py
@@ -1,12 +1,18 @@
-import pyNN.spiNNaker as p
+from pyNN.utility import get_script_args
+
+simulator_name = get_script_args(1)[0]
+exec("import pyNN.%s as p" % simulator_name)
+
 import pylab
 
 p.setup(timestep=1.0, min_delay=1.0, max_delay=144.0)
 nNeuronsPerCore = 10
 nNeurons = 15 * 4 * nNeuronsPerCore
-p.set_number_of_neurons_per_core("IF_curr_exp", nNeuronsPerCore)
-p.set_number_of_neurons_per_core("DelayExtension", nNeuronsPerCore)
 
+if simulator_name == "spiNNaker":
+    p.set_number_of_neurons_per_core("IF_curr_exp", nNeuronsPerCore)
+
+# Synfire Cell parameters
 cell_params_lif = {
     'cm': 0.25,
     'i_offset': 0.0,
@@ -19,35 +25,36 @@ cell_params_lif = {
     'v_thresh': -50.0
     }
 
-populations = list()
-projections = list()
+# Input Parameters
+spikeArray = {'spike_times': [[0]]}
 
+# Connection Parameters
 weight_to_spike = 2.0
-delay = 10
-
+delay = 2
 loopConnections = list()
 for i in range(0, nNeurons):
     singleConnection = (i, ((i + 1) % nNeurons), weight_to_spike, delay)
     loopConnections.append(singleConnection)
-
 injectionConnection = [(0, 0, weight_to_spike, 1)]
-spikeArray = {'spike_times': [[0]]}
-populations.append(
-        p.Population(nNeurons, p.IF_curr_exp, cell_params_lif, label='pop_1'))
-populations.append(
-        p.Population(1, p.SpikeSourceArray, spikeArray, label='inputSpikes_1'))
 
-projections.append(p.Projection(populations[0], populations[0],
-        p.FromListConnector(loopConnections)))
-projections.append(p.Projection(populations[1], populations[0],
-        p.FromListConnector(injectionConnection)))
+# Setup Populations
+synfire_population = p.Population(nNeurons, p.IF_curr_exp, cell_params_lif,
+        label='loop')
+input_population = p.Population(1, p.SpikeSourceArray, spikeArray,
+        label='input')
 
-populations[0].record()
+# Setup Projections
+synfire_projection = p.Projection(synfire_population, synfire_population,
+        p.FromListConnector(loopConnections))
+input_projection = p.Projection(input_population, synfire_population,
+        p.FromListConnector(injectionConnection))
 
-p.run(10000)
+# Record and run
+synfire_population.record()
+p.run(3000)
 
-spikes = populations[0].getSpikes(compatible_output=True)
-
+# Get and plot spikes
+spikes = synfire_population.getSpikes(compatible_output=True)
 pylab.figure()
 pylab.plot([i[1] for i in spikes], [i[0] for i in spikes], ".")
 pylab.xlabel('Time/ms')
@@ -55,4 +62,5 @@ pylab.ylabel('spikes')
 pylab.title('spikes')
 pylab.savefig("synfire_spikes.pdf", format="PDF")
 
+# End the Simulation
 p.end()
